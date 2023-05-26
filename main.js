29b0b50352ce468d7acbff006eeb911d54905f1c
Muhammad Hanzala <muhammadhanzala214@gmail.com>
2023-05-26 03:13:44 -0700

    snake is added

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const ctx  = canvas.getContext('2d');
 
 function drawGame() {
     clearScreen()
+    drawSnake()
+
+    let speed= 7; // interval will be seven times a second
+    setTimeout(drawGame, 1000/speed); // screen will update 7 times a sec
 }
 
 function clearScreen(){
@@ -14,5 +18,14 @@ function clearScreen(){
     ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight) // black color start from 0px left, right to canvas width and canvas height
 }
 
+let tileCount = 20;
+let headX = 10;
+let tileSize =18;
+let headY= 10;
 
- drawGame();
+function drawSnake(){
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(headX* tileCount,headY* tileCount,tileCount,tileSize);
+}
+
+drawGame();
